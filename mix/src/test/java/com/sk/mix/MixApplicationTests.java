3feabe58952695a1772944ead68796b0a8fe3dff
‘keasarsk’ <shaokaikkk@163.com>
2023-04-28 20:48:36 +0800

    start

---
+++ mix/src/test/java/com/sk/mix/MixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.mix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ routePlan/src/test/java/com/sk/routeplan/RoutePlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.routeplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutePlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
